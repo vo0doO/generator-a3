b3b3c01f40a132e62954700368e38f94cb4dd87d
Yun YoungSik <ysyun@yuwin.co.kr>
2015-09-15 15:10:29 +0900

    fix lodash export to _

---
+++ app/templates/client/sdk/redux/redux.service.js
@@ -0,0 +1,42 @@
+(function(Redux) {
+    'use strict';
+
+    angular
+        .module('ngRedux')
+        .service('reduxUtil', reduxUtil);
+
+    /* @ngInject */
+    function reduxUtil($ngRedux, $injector) {
+        this.mappingActionToScope = mappingActionToScope;
+        this.registerActionHandler = registerActionHandler;
+
+        function mappingActionToScope(mapStateScope, action, that) {
+            return $ngRedux.util($injector).connect(mapStateScope, action)(that);
+        }
+
+        function registerActionHandler(actionHandlers) {
+            var reducers = Redux.combineReducers(actionHandlers);
+            _createStore(reducers);
+        }
+
+        function _createStore(reducers) {
+            $ngRedux.createStoreWith(
+                reducers,
+                [_thunkMiddleware],
+                [] // dev-tools
+            );
+        }
+
+        function _thunkMiddleware(_ref) {
+            var dispatch = _ref.dispatch;
+            var getState = _ref.getState;
+
+            return function (next) {
+                return function (action) {
+                    return typeof action === 'function' ? action(dispatch, getState) : next(action);
+                };
+            };
+        }
+    }
+
+})(window.Redux);
